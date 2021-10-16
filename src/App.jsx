6ba2878cda42edacd72a30195c3395e5c10d1d56
Senimtra <senimtra@hotmail.com>
2021-10-16 13:13:28 +0200

    solve iteration 3: add new food

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import AddFood from './components/AddFood';
 import FoodBox from './components/FoodBox';
 import foods from './foods.json';
-import 'bulma/css/bulma.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div>IronNutrition</div>
+class App extends React.Component {
+  state = {
+    showAddFoodForm: false,
+    foodsList: foods,
+  };
 
-      {/* #################################
-          ##  Iteration 2: Display food  ##
-          ################################# */}
+  toggleAddFoodForm = () => {
+    this.setState({
+      showAddFoodForm: this.state.showAddFoodForm ? false : true,
+    });
+  };
 
-      {foods.map((food) => {
-        return <FoodBox food={food} />;
-      })}
-    </div>
-  );
+  addFood = (food) => {
+    this.setState({
+      foodsList: [
+        ...this.state.foodsList,
+        { name: food.name, calories: food.calories, image: food.image },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title is-1">IronNutrition</h1>
+        <button
+          id="add-food-btn"
+          className="button is-info"
+          onClick={this.toggleAddFoodForm}
+        >
+          Add new food
+        </button>
+        {this.state.showAddFoodForm && <AddFood onAddFood={this.addFood} />}
+        {/* #################################
+            ##  Iteration 2: Display food  ##
+            ################################# */}
+        {this.state.foodsList.map((food) => {
+          return <FoodBox key={Math.random()} food={food} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AddFood.jsx
@@ -0,0 +1,66 @@
+// #################################
+// ##  Iteration 3: Add new food  ##
+// #################################
+
+import React from 'react';
+
+class AddFood extends React.Component {
+  state = {
+    name: '',
+    calories: '',
+    image: '',
+  };
+
+  updateFood = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div id="add-food-form">
+        <h3 className="title is-4">Please add your food</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="input"
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.updateFood}
+            placeholder="Name of the food"
+          />
+          <input
+            className="input"
+            name="calories"
+            type="number"
+            value={this.state.calories}
+            onChange={this.updateFood}
+            placeholder="Containing calories"
+          />
+          <input
+            className="input"
+            name="image"
+            type="text"
+            value={this.state.image}
+            onChange={this.updateFood}
+            placeholder="Some nice picture of your food"
+          />
+          <button
+            className="button is-info"
+            onClick={() => this.props.onAddFood(this.state)}
+          >
+            Add food
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddFood;
